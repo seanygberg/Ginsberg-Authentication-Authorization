c0cee168083c46a49fe529e6c26839c50d884794
seanygberg <60160094+seanygberg@users.noreply.github.com>
2024-09-06 16:04:43 -0400

    first commit

---
+++ models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    username = db.Column(
+        db.String(20),
+        nullable=False,
+        unique=True,
+        primary_key=True,
+    )
+    password = db.Column(db.Text, nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
